6719636fdf3980a0b72f166d19ff95f7bbf1706c
LiYongjie <lyj02179318@alibaba-inc.com>
2025-04-18 18:49:42 +0800

    refactor(server): 修改 hello 路径的响应内容
    - 将 /hello 路径的响应内容从 "hello world" 修改为 "hello world hello world"

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('hello world');
+  res.send('hello world hello world');
 });
 
 app.listen(port, () => {
